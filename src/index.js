f6884ba0ba84f2bc221f21d6ede28f300b1bbdea
chandan <chandanprasad_d@trigent.com>
2025-02-19 21:04:44 +0530

    q

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-export {default as SimpleFeedbackWidget} from './floating-buttons/floating-button';
 
+export default function SimpleLoginWidget(props) {
+
+        return (
+          
+              <form className="slp-form-signin">
+                <h2 className="slp-text-center">ST CE Monitor</h2>
+                <label for="inputUserID" className="slp-sr-only">User ID</label>
+                <input type="text" id="inputUserID" className="slp-form-control" placeholder="User ID" required autofocus/>
+                <label for="inputPassword" clclassNamess="slp-sr-only">Password</label>
+                <input type="password" id="inputPassword" className="slp-form-control" placeholder="Password" required/>
+                <button className="slp-btn slp-btn-lg slp-tn-primary slp-btn-block" type="button">Sign in</button>
+                </form>
+         
+    
+       ) 
+}
