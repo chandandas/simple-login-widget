5a4056ec6db985715b25fbda1eb81e23df7c6512
chandan <chandanprasad_d@trigent.com>
2025-02-20 09:51:04 +0530

    1

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ export default function SimpleLoginWidget(props) {
 
         return (
           
-              <form className="slp-form-signin">
-                <h2 className="slp-text-center">ST CE Monitor</h2>
-                <label for="inputUserID" className="slp-sr-only">User ID</label>
-                <input type="text" id="inputUserID" className="slp-form-control" placeholder="User ID" required autofocus/>
-                <label for="inputPassword" clclassNamess="slp-sr-only">Password</label>
-                <input type="password" id="inputPassword" className="slp-form-control" placeholder="Password" required/>
-                <button className="slp-btn slp-btn-lg slp-tn-primary slp-btn-block" type="button">Sign in</button>
-                </form>
-         
+          <div className='login-form-bg container'>
+          <form className="form-signin">
+              <h2 className="text-center">ST CE Monitor</h2>
+              <label for="inputUserID" className="sr-only">User ID</label>
+              <input type="text" id="inputUserID" className="form-control" placeholder="User ID" required autofocus/>
+              <label for="inputPassword" className="sr-only">Password</label>
+              <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+              <button className="btn btn-lg btn-primary btn-block" type="button">Sign in</button>
+          </form>
+          </div>
     
        ) 
 }
