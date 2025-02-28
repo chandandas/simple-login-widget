a410bd9d029ace782cab78a0baa9c02d67fe25c2
chandan <chandanprasad_d@trigent.com>
2025-02-28 18:37:26 +0530

    1

--- src/login.js
+++ src/login.js
@@ -1,57 +1,46 @@
 "use client";
 
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { JSEncrypt } from "jsencrypt";
 
+export default  function LoginPage({title,bgImageUrl,flogin}){
 
-class LoginPage extends Component {
-    
-  constructor(props) {
-    super(props);
-    this.state = {
-        uid:  '',
-        pwd: ''
-    }
-    this.handleUidChange = this.handleUidChange.bind(this);
-    this.handlePwdChange = this.handlePwdChange.bind(this);
-    this.submit = this.submit.bind(this);
-  }
+  const [title,setTitle] = useState(title);
+  const [bgImageUrl,setBgImageUrl] = useState(bgImageUrl);
+  const [uid,setUid] = useState('');
+  const [pwd,setPwd] = useState('');
 
-
-  submit(){
-    const { login } = this.props;
-    login(this.state.uid,this.state.pwd);
-  }
-  handleUidChange(event) {
-   const newValue = event.target.value;
-   this.setState({ uid: newValue })
-}
-
-handlePwdChange(event){
-    const newValue = event.target.value;
+  useEffect(() => {
    
-    this.setState({ pwd: newValue })
-  }
-  render() {
+  }, [title, bgImageUrl]);
 
-    const { title,bgImageUrl } = this.props;
- 
-
-    return (
-        <div className='slw-login-form-bg container' style={{ backgroundImage: `url(${bgImageUrl})` }} >
-            <form className="slw-form-signin">
-                <h2 className="slw-text-center">{title}</h2>
-                <input type="text" className="slw-form-control" placeholder="User ID" 
-                    onChange={this.handleUidChange} value={this.state.uid} />
-                <input type="password" className="slw-form-control" placeholder="Password"  
-                onChange={this.handlePwdChange} value={this.state.pwd}  required/>
-                <button className="slw-btn slw-btn-lg slw-btn-primary slw-btn-block" type="button" onClick={this.submit}>Sign in</button>
-            </form>
-        </div>
-  
-    );
-  
+  function submit(){
+    const { login } = this.props;
+    login(uid,pwd);
   }
+  function  handleUidChange(event) {
+   const newValue = event.target.value;
+   setUid(newValue)
 }
 
-export default LoginPage;
+function handlePwdChange(event){
+    const newValue = event.target.value;
+    setPwd(newValue)
+  }
+
+  
+  return (
+    <div className='slw-login-form-bg container' style={{ backgroundImage: `url(${bgImageUrl})` }} >
+        <form className="slw-form-signin">
+            <h2 className="slw-text-center">{title}</h2>
+            <input type="text" className="slw-form-control" placeholder="User ID" 
+                onChange={handleUidChange} value={uid} />
+            <input type="password" className="slw-form-control" placeholder="Password"  
+            onChange={handlePwdChange} value={pwd}  required/>
+            <button className="slw-btn slw-btn-lg slw-btn-primary slw-btn-block" type="button" onClick={submit}>Sign in</button>
+        </form>
+    </div>
+
+);
+
+}
